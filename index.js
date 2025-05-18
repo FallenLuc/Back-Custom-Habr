7052c10aa1caf9385347f677b28a95308f118fed
Lucifer Meridian <fallen.luc.private@meridiansteam.com>
2025-05-18 16:44:20 +0300

    MDT-1412 add work server

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log('Happy developing ✨')
+const fs = require("fs")
+const jsonServer = require("json-server")
+const path = require("path")
+const https = require("https")
+const http = require("http")
+
+const server = jsonServer.create()
+
+const router = jsonServer.router(path.resolve(__dirname, "db.json"))
+
+server.use(jsonServer.defaults({}))
+server.use(jsonServer.bodyParser)
+
+server.use(async (req, res, next) => {
+	await new Promise(res => {
+		setTimeout(res, 800)
+	})
+	next()
+})
+
+server.post("/login", (req, res) => {
+	try {
+		const { userName, password } = req.body
+		const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8"))
+		const { users = [] } = db
+
+		const userFromBd = users.find(
+			user => user.userName === userName && user.password === password
+		)
+
+		if (userFromBd) {
+			return res.json({
+				userName: userFromBd.userName,
+				id: userFromBd.id,
+				avatar: userFromBd.avatar,
+				roles: userFromBd.roles,
+				features: userFromBd.features,
+				settings: userFromBd.settings
+			})
+		}
+
+		return res.status(403).json({ message: "User not found" })
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: e.message })
+	}
+})
+
+server.use((req, res, next) => {
+	next()
+})
+
+const HTTP_PORT = 8000
+const HTTPS_PORT = 8443
+
+const httpServer = http.createServer(server)
+
+server.use(router)
+
+httpServer.listen(HTTP_PORT, () => {
+	console.log(`server is running on ${HTTP_PORT} port`)
+})
